164a537a3494274223c22ce3151ce8633df440b1
Zhivche Gligorov <zhivche.gligorov@gmail.com>
2023-11-30 15:05:58 +0100

    Add Spring-boot application

---
+++ WineWithMe/src/test/java/mk/finki/ukim/dians/winewithme/WineWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.dians.winewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
