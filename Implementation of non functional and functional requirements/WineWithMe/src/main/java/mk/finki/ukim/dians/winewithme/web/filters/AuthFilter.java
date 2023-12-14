f48030719cefa1addd1e0e6ed55b326eaa1427d8
Zhivche Gligorov <zhivche.gligorov@gmail.com>
2023-12-14 23:23:39 +0100

    added Config and reconfigure filter blocking

--- Implementation of non functional and functional requirements/WineWithMe/src/main/java/mk/finki/ukim/dians/winewithme/web/filters/AuthFilter.java
+++ Implementation of non functional and functional requirements/WineWithMe/src/main/java/mk/finki/ukim/dians/winewithme/web/filters/AuthFilter.java
@@ -9,19 +9,26 @@ import mk.finki.ukim.dians.winewithme.model.User;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "AuthFilter",urlPatterns = {"/*"},
-        initParams = {@WebInitParam(name = "ignore-path1",value = "/login"),@WebInitParam(name = "ignore-path2",value = "/register"),
-                @WebInitParam(name = "ignore-path3",value = "/homepage"),@WebInitParam(name = "ignore-path4",value = "/auth-status")})
+@WebFilter(filterName = "AuthFilter", urlPatterns = {"/*"},
+        initParams = {@WebInitParam(name = "ignore-path1", value = "/login"),
+                @WebInitParam(name = "ignore-path2", value = "/register"),
+                @WebInitParam(name = "ignore-path3", value = "/homepage"),
+                @WebInitParam(name = "ignore-path4", value = "/auth-status"),
+                @WebInitParam(name = "ignore-path5", value = "/video"),
+                @WebInitParam(name = "ignore-path6", value = "/cssjs-dir")})
 public class AuthFilter implements Filter {
     private String[] ignorePaths;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        ignorePaths=new String[4];
-        ignorePaths[0]=filterConfig.getInitParameter("ignore-path1");
-        ignorePaths[1]=filterConfig.getInitParameter("ignore-path2");
-        ignorePaths[2]=filterConfig.getInitParameter("ignore-path3");
-        ignorePaths[3]=filterConfig.getInitParameter("ignore-path4");
+        ignorePaths = new String[6];
+        ignorePaths[0] = filterConfig.getInitParameter("ignore-path1");
+        ignorePaths[1] = filterConfig.getInitParameter("ignore-path2");
+        ignorePaths[2] = filterConfig.getInitParameter("ignore-path3");
+        ignorePaths[3] = filterConfig.getInitParameter("ignore-path4");
+        ignorePaths[4] = filterConfig.getInitParameter("ignore-path5");
+        ignorePaths[5] = filterConfig.getInitParameter("ignore-path6");
+
 
     }
 
@@ -30,16 +37,17 @@ public class AuthFilter implements Filter {
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpServletRequest req = (HttpServletRequest) servletRequest;
-        User user= (User) req.getSession().getAttribute("User");
-        String uri=req.getRequestURI();
+        User user = (User) req.getSession().getAttribute("User");
+        String uri = req.getRequestURI();
 
 
-        if(user!=null || uri.contains(ignorePaths[0]) || uri.contains(ignorePaths[1]) || uri.contains(ignorePaths[2])  || uri.contains(ignorePaths[3])){
-            filterChain.doFilter(req,resp);
-        }else{
+        if (user != null || uri.contains(ignorePaths[0]) || uri.contains(ignorePaths[1]) ||
+                uri.contains(ignorePaths[2]) || uri.contains(ignorePaths[3]) ||
+                uri.startsWith(ignorePaths[4]) || uri.startsWith(ignorePaths[5])) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
             resp.sendRedirect("/homepage");
         }
-
 
 
     }
